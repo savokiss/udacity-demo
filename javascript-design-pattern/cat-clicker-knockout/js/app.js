19810ddf5faf7456636f8d33a511c031781c9953
jason <jaynaruto@qq.com>
2016-11-28 20:17:48 +0800

    feature: cat click base

---
+++ javascript-design-pattern/cat-clicker-knockout/js/app.js
@@ -0,0 +1,11 @@
+var ViewModel = function(){
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+
+    this.incrementCounter = function(){
+        this.clickCount(this.clickCount()+1);
+    }.bind(this);
+}
+ko.applyBindings(new ViewModel());
