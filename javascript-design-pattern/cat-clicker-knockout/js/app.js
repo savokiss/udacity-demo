fc11595a3808af75ba4399085ce42a701b301297
jason <jaynaruto@qq.com>
2016-11-28 20:34:21 +0800

    feature: foreach

--- javascript-design-pattern/cat-clicker-knockout/js/app.js
+++ javascript-design-pattern/cat-clicker-knockout/js/app.js
@@ -1,11 +1,12 @@
 var ViewModel = function(){
     this.clickCount = ko.observable(0);
+    this.nicknames = ['a','b','savo','xy'];
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
 
     this.level = ko.computed(function(){
-        return this.clickCount() < 10 ? 'newBorn' : 'teen';
+        return this.clickCount() < 10 ? 'Newborn' : 'Teen';
     }, this);
     this.incrementCounter = function(){
         this.clickCount(this.clickCount()+1);
