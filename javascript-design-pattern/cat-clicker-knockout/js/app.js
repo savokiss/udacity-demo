bb064dd499b5c3a080abb4ec32f708aca66f4ab7
savokiss <jaynaruto@qq.com>
2017-05-02 20:51:41 +0800

    feat: Cat Constructor

--- javascript-design-pattern/cat-clicker-knockout/js/app.js
+++ javascript-design-pattern/cat-clicker-knockout/js/app.js
@@ -1,15 +1,19 @@
-var ViewModel = function(){
+var Cat = function () {
     this.clickCount = ko.observable(0);
-    this.nicknames = ['Tom','Jerry','savo','xiaoyan'];
+    this.nicknames = ['Tom', 'Jerry', 'savo', 'xiaoyan'];
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
-
-    this.level = ko.computed(function(){
+    this.level = ko.computed(function () {
         return this.clickCount() < 10 ? 'Newborn' : 'Teen';
     }, this);
-    this.incrementCounter = function(){
-        this.clickCount(this.clickCount()+1);
-    }.bind(this);
+}
+
+var ViewModel = function () {
+    this.currentCat = ko.observable(new Cat());
+
+    this.incrementCounter = function () {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
 }
 ko.applyBindings(new ViewModel());
