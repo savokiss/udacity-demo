7b418ec91e421224485d09e131fdc510f68addc5
savokiss <jaynaruto@qq.com>
2017-05-03 21:35:45 +0800

    feat: cat clicker knockout finish

--- javascript-design-pattern/cat-clicker-knockout/js/app.js
+++ javascript-design-pattern/cat-clicker-knockout/js/app.js
@@ -1,17 +1,25 @@
-var Cat = function () {
-    this.clickCount = ko.observable(0);
-    this.nicknames = ['Tom', 'Jerry', 'savo', 'xiaoyan'];
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
-    this.imgAttribution = ko.observable('https://www.flickr.com/photos/big');
+var Cat = function (options) {
+    this.clickCount = ko.observable(options.clickCount);
+    this.nicknames = ko.observable(options.nicknames);
+    this.name = ko.observable(options.name);
+    this.imgSrc = ko.observable(options.imgSrc);
+    this.imgAttribution = ko.observable(options.imgAttribution);
     this.level = ko.computed(function () {
         return this.clickCount() < 10 ? 'Newborn' : 'Teen';
     }, this);
 }
 
 var ViewModel = function () {
-    this.currentCat = ko.observable(new Cat());
+    var self = this;
+    this.catList = ko.observableArray([]);
+    initialCats.forEach(function (catItem) {
+        self.catList.push(new Cat(catItem));
+    }.bind(this))
+    this.currentCat = ko.observable(this.catList()[0]);
 
+    this.setCat = function(clickedCat) {
+        self.currentCat(clickedCat)
+    }
     this.incrementCounter = function () {
         this.currentCat().clickCount(this.currentCat().clickCount() + 1);
     }.bind(this) // this的作用域会根据页面中的 with 而变化
---
+++ javascript-design-pattern/cat-clicker-knockout/js/cats.js
@@ -0,0 +1,19 @@
+var initialCats = [{
+    clickCount: 0,
+    name: 'Savo',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/big',
+    nicknames: ['savo', 'test', 'savokiss', 'xiaoyan']
+}, {
+    clickCount: 0,
+    name: 'Kiss',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/big',
+    nicknames: ['savo', 'test', 'savokiss', 'xiaoyan']
+}, {
+    clickCount: 0,
+    name: 'Xiaoyan',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    imgAttribution: 'https://www.flickr.com/photos/big',
+    nicknames: ['savo', 'test', 'savokiss', 'xiaoyan']
+}]
